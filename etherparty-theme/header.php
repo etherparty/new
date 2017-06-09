dd30d09be42049c092ba1d9c53a3fb8d4d62e13b
Tulun <jaykiraly@gmail.com>
2017-06-08 19:06:13 -0700

    Added in jQuery cdn, bootstrap JS; added in basic navbar.

--- etherparty-theme/header.php
+++ etherparty-theme/header.php
@@ -16,6 +16,10 @@
   ================================================== -->
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <?php wp_head(); ?>
   <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,700,300,600,800,400' rel='stylesheet' type='text/css'>
@@ -33,7 +37,7 @@
 </head>
 <body>
   <img class="img-responsive logo-at-top" src="/wp-content/themes/etherparty-theme/img/etherparty_logo.png" alt="etherparty-logo" />
-  <nav class="navbar main-navigation">
+<!--   <nav class="navbar main-navigation">
       <div class="navigation-menu-item"><a href="#">Home</a></div>
       <div class="navigation-menu-item"><a href="#">Enterprise</a></div>
       <div class="navigation-menu-item"><a href="#">Use Cases</a></div>
@@ -41,4 +45,32 @@
       <div class="navigation-menu-item"><a href="#">ICO</a></div>
       <div class="navigation-menu-item"><a href="#">Blog</a></div>
       <div class="navigation-menu-item"><a href="#">Contact</a></div>
+  </nav> -->
+
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#ether-nav" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <!-- <a class="navbar-brand" href="#">Brand</a> -->
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="ether-nav">
+        <ul class="nav nav-pills nav-justified">
+          <li class="active"><a href="#">Home<span class="sr-only">(current)</span></a></li>
+          <li><a href="#">Enterprise</a></li>
+          <li><a href="#">Use Cases</a></li>
+          <li><a href="#">Resources</a></li>
+          <li><a href="#">ICO</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
   </nav>
