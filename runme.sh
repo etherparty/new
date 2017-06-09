e80a1d905b6ad243eebc9e29e2c9f8c3423283c8
Brian Onn <brian@vanbex.com>
2017-06-08 17:45:55 -0700

    updaded runme

--- runme.sh
+++ runme.sh
@@ -3,7 +3,15 @@
 USER=$(whoami)
 sudo apt-get install docker docker-compose
 docker-compose up -d
-sleep 5
+
+x=20
+while test $x -gt 0; do
+    if test -d wp-content/themes; then
+        sudo ln -s ../../etherparty-theme .
+        x=0
+    fi;
+    x=$((x - 1))
+    sleep 5;
+done
+
 sudo chmod -R "$USER" .
-cd wp-content/themes
-ln -s ../../new/etherparty-theme ./etherparty-theme
