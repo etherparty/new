56e93e0cd49001b259ef1dc3b4f1a56b76d10204
William Choi <wchoi95@gmail.com>
2017-06-12 16:05:42 -0700

    Fixed footer padding issue

--- etherparty-theme/footer.php
+++ etherparty-theme/footer.php
@@ -1,39 +1,41 @@
   <footer class="footer">
-    <div class='row'>
-      <div class='col-xs-12 col-sm-4 col-md-4'>
-        <p class='footer-text text-center'>
-          Etherparty <br />
-          717 West Pender St, <br />
-          Vancouver, BC, <br />
-          V6C 2X6 <br />
-        </p>
-        <p class='footer-text text-center'>
-          Follow Us Here
-        </p>
-        <p class='footer-text text-center'>
-          <a class='social-icons' href="http://facebook.com/etherparty" target="_blank"><i class="fa fa-facebook-square social"></i></a>
-          <a class='social-icons' href="http://twitter.com/etherparty_io" target="_blank"><i class="fa fa-twitter-square social"></i></a>
-        </p>
-      </div>
-      <div class='col-xs-12 col-sm-4 col-md-4 text-center'>
-        <p class="footer-text text-center">Copyright © 2017 Etherparty</p>
-        <button class='footer-btn btn btn-primary' type='button'>
-          <a href='http://eepurl.com/bxE4Sn'>Subscribe</a>
-        </button>
-      </div>
-      <div class='col-xs-12 col-sm-4 col-md-4 text-center'>
-        <p class='footer-text'>
-          Links
-        </p>
-        <button class="text-center footer-btn">
-          <a href="mailto:etherparty@vanbex.com" role="button">Contact Us</a>
-        </button>
-        <button class="text-center footer-btn">
-          <a href="#" role="button">Link 2</a>
-        </button>
-        <button class="text-center footer-btn">
-          <a href="#" role="button">Link 3</a>
-        </button>
+    <div class="container-fluid">
+      <div class='row'>
+        <div class='col-xs-12 col-sm-4 col-md-4'>
+          <p class='footer-text text-center'>
+            Etherparty <br />
+            717 West Pender St, <br />
+            Vancouver, BC, <br />
+            V6C 2X6 <br />
+          </p>
+          <p class='footer-text text-center'>
+            Follow Us Here
+          </p>
+          <p class='footer-text text-center'>
+            <a class='social-icons' href="http://facebook.com/etherparty" target="_blank"><i class="fa fa-facebook-square social"></i></a>
+            <a class='social-icons' href="http://twitter.com/etherparty_io" target="_blank"><i class="fa fa-twitter-square social"></i></a>
+          </p>
+        </div>
+        <div class='col-xs-12 col-sm-4 col-md-4 text-center'>
+          <p class="footer-text text-center">Copyright © 2017 Etherparty</p>
+          <button class='footer-btn btn btn-primary' type='button'>
+            <a href='http://eepurl.com/bxE4Sn'>Subscribe</a>
+          </button>
+        </div>
+        <div class='col-xs-12 col-sm-4 col-md-4 text-center'>
+          <p class='footer-text'>
+            Links
+          </p>
+          <button class="text-center footer-btn">
+            <a href="mailto:etherparty@vanbex.com" role="button">Contact Us</a>
+          </button>
+          <button class="text-center footer-btn">
+            <a href="#" role="button">Link 2</a>
+          </button>
+          <button class="text-center footer-btn">
+            <a href="#" role="button">Link 3</a>
+          </button>
+        </div>
       </div>
     </div>
   </footer>
